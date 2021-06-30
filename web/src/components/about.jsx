cf8a326a29e3e4555003599e444ec058cf2fc5f1
kyleolson812 <kyleolson812@gmail.com>
2021-06-29 22:28:08 -0400

    Implemented first about page

--- web/src/components/about.jsx
+++ web/src/components/about.jsx
@@ -1,5 +1,12 @@
+import style from '../styles/about.module.scss'
+
 export default function About() {
   return (
-    <h1>About</h1>
+    <div className={style.testing}>
+      <h1 id={style.title}>About Midpoints</h1>
+      
+      <h2 id={style.section}>Midpoint is a website where a user can enter points of interest to them and in return
+      the website will generate great meeting locations between each point of interest</h2>
+    </div>
   )
 }
