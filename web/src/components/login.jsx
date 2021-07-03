225734ab3f0d89b80302c73f1a2619698f637f38
Vijay Stroup <vijaystroup@gmail.com>
2021-07-03 11:54:52 -0400

    login form

--- web/src/components/login.jsx
+++ web/src/components/login.jsx
@@ -1,5 +1,45 @@
+import { Formik, Form, Field } from 'formik'
+import * as Yup from 'yup'
+import style from '../styles/login.module.scss'
+
 export default function Login() {
   return (
-    <h1>Login</h1>
+    <LoginForm />
+  )
+}
+
+const LoginSchema = Yup.object().shape({
+  email: Yup.string().email('Invalid email').required('Required'),
+  password: Yup.string().required('Required')
+})
+function LoginForm() {
+  return (
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      validationSchema={LoginSchema}
+      onSubmit={async (values, { resetForm }) => {
+        alert(values)
+        resetForm()
+      }}
+    >
+      {({ errors, touched }) => (
+        <Form>
+          <div className={style.field}>
+            <p>Email</p>
+            <Field name='email' type='email' />
+            {errors.email && touched.email ? <p className={style.error}>{errors.email}</p> : null}
+          </div>
+
+          <div className={style.field}>
+            <p>Password</p>
+            <Field name='password' type='password' />
+            {errors.password && touched.password ? <p className={style.error}>{errors.password}</p> : null}
+          </div>
+
+          <div className={style.btn}><button type='submit'>Login</button></div>
+          <div className={style.btn}><button>Forgot your password?</button></div>
+        </Form>
+      )}
+    </Formik>
   )
 }
