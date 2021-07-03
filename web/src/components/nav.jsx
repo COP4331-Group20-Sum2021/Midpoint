79879f3be7b9679220fb6370fbb3e9a8e80dec54
Vijay Stroup <vijaystroup@gmail.com>
2021-07-03 11:54:35 -0400

    add alt to img to supress warning

--- web/src/components/nav.jsx
+++ web/src/components/nav.jsx
@@ -4,7 +4,7 @@ import style from '../styles/nav.module.scss'
 export default function Nav() {
   return (
     <nav className={style.nav}>
-      <img src='/logo.png' id={style.navLogo} />
+      <img src='/logo.png' id={style.navLogo} alt='logo' />
       <ul className={style.navItems}>
         <Link to='/' className={style.navItem}><li>Home</li></Link>
         <Link to='/about' className={style.navItem}><li>About</li></Link>
