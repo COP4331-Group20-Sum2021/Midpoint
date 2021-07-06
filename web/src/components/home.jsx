6a22a78e02a7aaf952cbc3c1d7b05c76e4b9b014
Devin Besaw <devinbesaw15@yahoo.com>
2021-07-06 01:35:46 -0400

    styling adjustments

--- web/src/components/home.jsx
+++ web/src/components/home.jsx
@@ -1,25 +1,19 @@
 import "../styles/Home.scss"
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
-import styled from "styled-components";
 import { Earth } from "./earth/earth";
-
-const CanvasContainer = styled.div`
-  width: 100%;
-  height: 600px;
-  `;
 
 export default function Home() {
   return (
     <div className="content">
 
-      <CanvasContainer className="earth">
+      <div className="earth">
         <Canvas>
           <Suspense fallback={null}>
               <Earth/>
           </Suspense>
         </Canvas>
-      </CanvasContainer>
+      </div>
 
       <div className="container">
 
