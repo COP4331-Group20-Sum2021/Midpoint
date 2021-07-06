60df70e4cca80977c210cd754655f07476dde7a8
Joseph Terribile <kartingman25@gmail.com>
2021-07-05 22:54:26 -0400

    Setup static serving of HTML

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 5000; 
 
@@ -11,4 +12,12 @@ app.set('port', (process.env.PORT || 5000));
 app.use(cors());
 app.use(bodyParser.json());
 
-app.listen(PORT, () => {  console.log('Server listening on port ' + PORT);});
+if (process.env.NODE_ENV === 'production') {  
+    // Set static folder
+    app.use(express.static(path.join(__dirname, '..', 'web', 'build')));
+    app.get('*', (req, res) =>  {
+        res.sendFile(path.resolve(__dirname, '..', 'web', 'build', 'index.html'));  
+    });
+}
+
+app.listen(PORT, () => {console.log('Server listening on port ' + PORT);});
