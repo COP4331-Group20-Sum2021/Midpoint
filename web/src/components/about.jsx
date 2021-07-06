bffa9760c53eee25172975ad8d5c1173f2c3f30b
kyleolson812 <kyleolson812@gmail.com>
2021-07-06 10:54:54 -0400

    About section completed

--- web/src/components/about.jsx
+++ web/src/components/about.jsx
@@ -2,8 +2,16 @@ import style from '../styles/about.module.scss'
 
 export default function About() {
   return (
+    <>
+      <img id={style.gmap} src='/googlemaps_3.png' alt='google maps' />
+      <AboutPage />
+    </>
+  )
+}
+function AboutPage() {
+  return (
     <div className={style.aboutBlock}>
-      <div className={style.test}>
+      <div className={style.textBox}>
         <h1 id={style.title}>About Midpoint</h1>
       
         <h2 id={style.section}>Midpoint is a website where a user can enter points of interest to them and in return
--- web/src/components/background.jsx
+++
@@ -1,9 +0,0 @@
-import style from '../styles/background.module.scss'
-
-export default function Background() {
-  return (
-    <div className={style.background}>
-        <img src='/maps.png' id={style.background} />
-    </div>
-  )
-}
