2b88d81f60d75c27bb4102af98bf02e1900761e4
Vijay Stroup <vijaystroup@gmail.com>
2021-07-08 13:17:40 -0400

    login/logout on user state

--- web/src/components/nav.jsx
+++ web/src/components/nav.jsx
@@ -1,14 +1,20 @@
 import { Link } from 'react-router-dom'
+import { useAuth } from '../contexts/authContext'
 import style from '../styles/nav.module.scss'
 
 export default function Nav() {
+  const { user, logout } = useAuth()
+
   return (
     <nav className={style.nav}>
       <img src='/logo.png' id={style.navLogo} alt='logo' />
       <ul className={style.navItems}>
         <Link to='/' className={style.navItem}><li>Home</li></Link>
         <Link to='/about' className={style.navItem}><li>About</li></Link>
-        <Link to='/signup' className={style.navItem}><li>Signup</li></Link>
+        {user
+          ? <Link to='/' className={style.navItem}><li onClick={logout}>Logout</li></Link>
+          : <Link to='/login' className={style.navItem}><li>Login</li></Link>
+        }
       </ul>
     </nav>
   )
