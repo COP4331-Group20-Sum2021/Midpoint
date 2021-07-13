140844df8b07278bf09a4a0fa18397a36e6970ae
Vijay Stroup <vijaystroup@gmail.com>
2021-07-13 18:13:24 -0400

    register content

--- mobile/components/register.jsx
+++ mobile/components/register.jsx
@@ -1,10 +1,74 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Button, TextInput, StyleSheet } from 'react-native';
+import { Formik } from 'formik';
 
 export default function Register({ navigation }) {
     return (
-        <View>
-            <Text>This is the register screen in "components/register.jsx"</Text>
-        </View>
+        <Formik
+            initialValues={{ email: '' }}
+            onSubmit={values => console.log(values)}
+        >
+        {({ handleChange, handleBlur, handleSubmit, values }) => (
+            <View style={style.container}>
+                <View style={style.section}>
+                    <Text style={style.fieldName}>Email</Text>
+                    <TextInput
+                        onChangeText={handleChange('email')}
+                        onBlur={handleBlur('email')}
+                        value={values.email}
+                        style={style.input}
+                    />
+                </View>
+                <View style={style.section}>
+                    <Text style={style.fieldName}>Password</Text>
+                    <TextInput
+                        onChangeText={handleChange('password')}
+                        onBlur={handleBlur('password')}
+                        value={values.password}
+                        style={style.input}
+                    />
+                </View>
+                <View style={style.section}>
+                    <Text style={style.fieldName}>Confirm Password</Text>
+                    <TextInput
+                        onChangeText={handleChange('passwordConfirm')}
+                        onBlur={handleBlur('passwordConfirm')}
+                        value={values.passwordConfirm}
+                        style={style.input}
+                    />
+                </View>
+                <View style={style.section}>
+                    <Button style={style.submit} onPress={handleSubmit} title="Submit" />
+                </View>
+            </View>
+        )}
+        </Formik>
     )
 }
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingLeft: 10,
+        paddingRight: 10,
+        marginTop: 10,
+    },
+    section: {
+        marginTop: 20,
+    },
+    input: {
+        shadowColor: 'black',
+        shadowOpacity: .1,
+        shadowRadius: 5,
+        borderRadius: 5,
+        fontSize: 20,
+    },
+    fieldName: {
+        fontSize: 20,
+    },
+    submit: {
+        fontSize: 30,
+        margin: 10,
+        borderRadius: 5,
+    }
+});
