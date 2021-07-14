6c4adc38409e2b6ce3aa2b3faee47c563e0b2d60
Joseph Terribile <kartingman25@gmail.com>
2021-07-14 13:01:46 -0400

    Create groupCRUD.js skeleton

---
+++ backend/api/groupCRUD.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { admin, db } = require('../auth/firebase');
+
+const router = express.Router();
+
+// = = = = = = = = = = API HERE = = = = = = = = = = 
+
+
+
+// = = = = = = = = = = API ENDS = = = = = = = = = = 
+
+module.exports = router;
--- backend/api/locationapi.js
+++ backend/api/locationapi.js
@@ -2,7 +2,6 @@ const express = require('express');
 const axios = require('axios');
 const { admin, db } = require('../auth/firebase');
 const { Navigator } = require('node-navigator');
-require('dotenv').config();
 
 const router = express.Router();
 
