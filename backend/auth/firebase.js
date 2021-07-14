8c77e05ace1f283ce82a9ca6f329417d3b35640e
Nathan Kurelo Wilk <40876504+Nate8888@users.noreply.github.com>
2021-07-14 14:57:36 -0400

    Update firebase.js

--- backend/auth/firebase.js
+++ backend/auth/firebase.js
@@ -23,11 +23,17 @@ const authJson = (function() {
 //const admin = firebase.initializeApp(authJson);
 const admin = firebase.initializeApp({
   credential: firebase.credential.cert({
-    "projectId": process.env.BACKEND_FIREBASE_PROJECT_ID,
-    "private_key": process.env.BACKEND_FIREBASE_PRIVATE_KEY,
-    "client_email": process.env.BACKEND_FIREBASE_CLIENT_EMAIL,
-  }),
-  databaseURL: "https://group20-midpoint.firebaseio.com"
+            "type": process.env.BACKEND_FIREBASE_TYPE,
+            "project_id": process.env.BACKEND_FIREBASE_PROJECT_ID,
+            "private_key_id": process.env.BACKEND_FIREBASE_PRIVATE_KEY_ID,
+            "private_key": process.env.BACKEND_FIREBASE_PRIVATE_KEY,
+            "client_email": process.env.BACKEND_FIREBASE_CLIENT_EMAIL,
+            "client_id": process.env.BACKEND_FIREBASE_CLIENT_ID,
+            "auth_uri": process.env.BACKEND_FIREBASE_AUTH_URI,
+            "token_uri": process.env.BACKEND_FIREBASE_TOKEN_URI,
+            "auth_provider_x509_cert_url": process.env.BACKEND_FIREBASE_AUTH_PROVIDER_X509,
+            "client_x509_cert_url": process.env.BACKEND_FIREBASE_CLIENT_X509
+  })
 });
 
 const auth = admin.auth();
