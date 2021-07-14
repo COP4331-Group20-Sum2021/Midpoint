cdcbb14582213c2a8d49f2e8010b9b1d4e4944c0
Nathan Kurelo Wilk <40876504+Nate8888@users.noreply.github.com>
2021-07-14 15:06:14 -0400

    restore previous main branch

--- backend/auth/firebase.js
+++ backend/auth/firebase.js
@@ -19,22 +19,7 @@ const authJson = (function() {
     return require('../serviceAccountKey.json');
 })();
 
-// commented this line that JOseph had
-//const admin = firebase.initializeApp(authJson);
-const admin = firebase.initializeApp({
-  credential: firebase.credential.cert({
-            "type": process.env.BACKEND_FIREBASE_TYPE,
-            "project_id": process.env.BACKEND_FIREBASE_PROJECT_ID,
-            "private_key_id": process.env.BACKEND_FIREBASE_PRIVATE_KEY_ID,
-            "private_key": process.env.BACKEND_FIREBASE_PRIVATE_KEY,
-            "client_email": process.env.BACKEND_FIREBASE_CLIENT_EMAIL,
-            "client_id": process.env.BACKEND_FIREBASE_CLIENT_ID,
-            "auth_uri": process.env.BACKEND_FIREBASE_AUTH_URI,
-            "token_uri": process.env.BACKEND_FIREBASE_TOKEN_URI,
-            "auth_provider_x509_cert_url": process.env.BACKEND_FIREBASE_AUTH_PROVIDER_X509,
-            "client_x509_cert_url": process.env.BACKEND_FIREBASE_CLIENT_X509
-  })
-});
+const admin = firebase.initializeApp(authJson);
 
 const auth = admin.auth();
 const db = admin.firestore();
