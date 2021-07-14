860999e31cc3d79635b5c9920628a7dbd26e7513
Nate8888 <wilk.nathan@gmail.com>
2021-07-14 14:38:23 -0700

    basic remove myself from group

--- backend/api/groupCRUD.js
+++ backend/api/groupCRUD.js
@@ -5,18 +5,40 @@ const router = express.Router();
 
 // = = = = = = = = = = API HERE = = = = = = = = = = 
 
+// Add participant userId to groupId
 router.post('/addparticipant', async (req, res, next) => {
     const {userId, userToken, groupId} = req.body;
     var status = 200;
     var error = '';
 
+    // Todo: Check userToken
+    // Todo: Check if userid is a valid entry
+    // Todo: Check if groupId is a valid group
+    
     const data = {
         groupid: groupId,
         userid: userId
     };
       
       // Add a new document in collection "groupmember" with auto-ID
-    const response = await db.collection('groupmember').doc().set(data);
+    const response = await db.collection('groupmember').doc(userId+groupId).set(data);
+
+    var ret = { error: error };
+
+    res.status(status).json(ret);
+});
+
+// Add participant userId to groupId
+router.post('/removemyself', async (req, res, next) => {
+    const {userId, userToken, groupId} = req.body;
+    var status = 200;
+    var error = '';
+
+    // Todo: Check userToken
+    // Todo: Check if userid is a valid entry
+    // Todo: Check if groupId is a valid group
+
+    const response = await db.collection('groupmember').doc(userId+groupId).delete();
 
     var ret = { error: error };
 
