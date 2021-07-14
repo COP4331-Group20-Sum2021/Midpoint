920037f5baaac351f6d86e91b41a92b357228fc6
Vijay Stroup <vijaystroup@gmail.com>
2021-07-14 18:20:02 -0400

    register with firebase

--- mobile/components/register.jsx
+++ mobile/components/register.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Text, View, Button, TextInput, StyleSheet } from 'react-native';
 import { Formik } from 'formik';
 import * as yup from 'yup';
+import { useAuth } from '../context/AuthContext';
+import { useState } from 'react';
 
 const loginSchema = yup.object().shape({
     email: yup
@@ -19,47 +21,67 @@ const loginSchema = yup.object().shape({
 })
 
 export default function Register({ navigation }) {
+    const { signup } = useAuth()
+    const [clicked, setClicked] = useState(false)
+
     return (
         <Formik
             initialValues={{ email: '', password: '', passwordConfirm: '' }}
             validationSchema={loginSchema}
-            onSubmit={values => console.log(values)}
+            onSubmit={async (values, { resetForm }) => {
+                try {
+                  await signup(values.email, values.password)
+                  setClicked(true)
+                } catch(e) {
+                  alert('Failed to Signup: ' + e)
+                }
+                resetForm()
+            }}
         >
         {({ handleChange, handleBlur, handleSubmit, values, errors, touched, isValid}) => (
             <View style={style.container}>
-                <View style={style.section}>
-                    <Text style={style.fieldName}>Email</Text>
-                    <TextInput
-                        onChangeText={handleChange('email')}
-                        onBlur={handleBlur('email')}
-                        value={values.email}
-                        style={style.input}
-                    />
-                    {(errors.email && touched.email) && <Text style={style.error}>{errors.email}</Text>}
-                </View>
-                <View style={style.section}>
-                    <Text style={style.fieldName}>Password</Text>
-                    <TextInput
-                        onChangeText={handleChange('password')}
-                        onBlur={handleBlur('password')}
-                        value={values.password}
-                        style={style.input}
-                    />
-                    {(errors.password && touched.password) && <Text style={style.error}>{errors.password}</Text>}
-                </View>
-                <View style={style.section}>
-                    <Text style={style.fieldName}>Confirm Password</Text>
-                    <TextInput
-                        onChangeText={handleChange('passwordConfirm')}
-                        onBlur={handleBlur('passwordConfirm')}
-                        value={values.passwordConfirm}
-                        style={style.input}
-                    />
-                    {(errors.passwordConfirm && touched.passwordConfirm) && <Text style={style.error}>{errors.passwordConfirm}</Text>}
-                </View>
-                <View style={style.section}>
-                    <Button style={style.submit} onPress={handleSubmit} title="Submit" />
-                </View>
+                {!clicked && <>
+                    <View style={style.section}>
+                        <Text style={style.fieldName}>Email</Text>
+                        <TextInput
+                            onChangeText={handleChange('email')}
+                            onBlur={handleBlur('email')}
+                            value={values.email}
+                            style={style.input}
+                        />
+                        {(errors.email && touched.email) && <Text style={style.error}>{errors.email}</Text>}
+                    </View>
+                    <View style={style.section}>
+                        <Text style={style.fieldName}>Password</Text>
+                        <TextInput
+                            onChangeText={handleChange('password')}
+                            onBlur={handleBlur('password')}
+                            value={values.password}
+                            style={style.input}
+                        />
+                        {(errors.password && touched.password) && <Text style={style.error}>{errors.password}</Text>}
+                    </View>
+                    <View style={style.section}>
+                        <Text style={style.fieldName}>Confirm Password</Text>
+                        <TextInput
+                            onChangeText={handleChange('passwordConfirm')}
+                            onBlur={handleBlur('passwordConfirm')}
+                            value={values.passwordConfirm}
+                            style={style.input}
+                        />
+                        {(errors.passwordConfirm && touched.passwordConfirm) && <Text style={style.error}>{errors.passwordConfirm}</Text>}
+                    </View>
+                    <View style={style.section}>
+                        <Button style={style.submit} onPress={handleSubmit} title="Submit" />
+                    </View>
+                </>
+                }
+                {clicked && <>
+                    <View style={style.verifyContainer}>
+                        <Text style={style.verify}>Check your email to verify your account!</Text>
+                        <Text style={style.verify}>p.s. don't forget about your spam folder.</Text>
+                    </View>
+                </>}
             </View>
         )}
         </Formik>
@@ -95,5 +117,13 @@ const style = StyleSheet.create({
         fontSize: 30,
         margin: 10,
         borderRadius: 5,
+    },
+    verifyContainer: {
+        marginTop: 10,
+        textAlign: 'center',
+    },
+    verify: {
+        fontSize: 18,
+        color: 'blue',
     }
 });
