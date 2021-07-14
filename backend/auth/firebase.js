c186e3dc32f6e28bfb440a616802616697ba4545
Joseph Terribile <kartingman25@gmail.com>
2021-07-14 14:30:30 -0400

    Spelling fix

--- backend/auth/firebase.js
+++ backend/auth/firebase.js
@@ -4,10 +4,10 @@ const authJson = (function() {
     // if on heroku, do generate json from env vars
     if (process.env.NODE_ENV === 'production') {
         return {
-            "type": process.env.BACKNED_FIREBASE_TYPE,
-            "project_id": process.env.BACKNED_FIREBASE_PROJECT_ID,
-            "private_key_id": process.env.BACKNED_FIREBASE_PRIVATE_KEY_ID,
-            "private_key": process.env.BACKNED_FIREBASE_PRIVATE_KEY,
+            "type": process.env.BACKEND_FIREBASE_TYPE,
+            "project_id": process.env.BACKEND_FIREBASE_PROJECT_ID,
+            "private_key_id": process.env.BACKEND_FIREBASE_PRIVATE_KEY_ID,
+            "private_key": process.env.BACKEND_FIREBASE_PRIVATE_KEY,
             "client_email": process.env.BACKEND_FIREBASE_CLIENT_EMAIL,
             "client_id": process.env.BACKEND_FIREBASE_CLIENT_ID,
             "auth_uri": process.env.BACKEND_FIREBASE_AUTH_URI,
