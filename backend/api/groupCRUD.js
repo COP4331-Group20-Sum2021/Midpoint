65a4c136e8d4dba20cc1040908993bcf53abca5f
Nate8888 <wilk.nathan@gmail.com>
2021-07-14 14:19:48 -0700

    Simple add participant

--- backend/api/groupCRUD.js
+++ backend/api/groupCRUD.js
@@ -5,6 +5,24 @@ const router = express.Router();
 
 // = = = = = = = = = = API HERE = = = = = = = = = = 
 
+router.post('/addparticipant', async (req, res, next) => {
+    const {userId, userToken, groupId} = req.body;
+    var status = 200;
+    var error = '';
+
+    const data = {
+        groupid: groupId,
+        userid: userId
+    };
+      
+      // Add a new document in collection "groupmember" with auto-ID
+    const response = await db.collection('groupmember').doc().set(data);
+
+    var ret = { error: error };
+
+    res.status(status).json(ret);
+});
+
 
 
 // = = = = = = = = = = API ENDS = = = = = = = = = = 
