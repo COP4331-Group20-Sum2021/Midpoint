a5b9e12e08baa6081365078e4c2c1d710366d16c
Vijay Stroup <vijaystroup@gmail.com>
2021-07-23 15:59:52 -0400

    verify refresh page reminder

--- web/src/components/verify.jsx
+++ web/src/components/verify.jsx
@@ -4,6 +4,7 @@ export default function Verify() {
   return (
     <div className={style.container}>
       <h1>Please verify your accout through your email to get started.</h1>
+      <p>If you have verifed your account and are still being redirected, refresh the page.</p>
     </div>
   )
 }
