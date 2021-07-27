8f6aecfabf26b3f8fdda55ac4a247657474b1ade
Devin Besaw <devinbesaw15@yahoo.com>
2021-07-27 17:50:29 -0400

    invitation table set up. profile picture centered.

--- web/src/components/invitations.jsx
+++ web/src/components/invitations.jsx
@@ -1,11 +1,49 @@
+import React, { useEffect, useState } from "react";
 import SideBar from "./sidebar"
 import "../styles/invitations.scss";
 import { useAuth } from "../contexts/authContext";
+import { getDynamicStyles } from "jss";
 
 
 
-export default function Map() {
-  const { user } = useAuth()
+export default function Invitations() {
+  const { user } = useAuth();
+  const [invitations, setInvitations] = useState([
+    {
+      email: "test@gmail.com"
+    },
+    {
+      email: "test@gmail.com"
+    }
+  ]);
+
+  // useEffect(() => {
+  //   fetch('https://group20-midpoint.herokuapp.com/api/listinvites', {
+  //     method: "POST",
+  //     headers: {
+  //       'Accept': 'application/json',
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({
+  //         userId : user.uid,
+  //         userToken : user.Aa,
+  //         email: user.email,
+  //       })
+  //   })
+  //   .then(response => response.json())
+  //   .then(data => {console.log(data);setInvitations(data.invitedata)})
+  // }, []);
+
+  function acceptInvitation()
+  {
+    console.log("invitation accepted :)");
+  }
+
+  function denyInvitation()
+  {
+    console.log("invitation denied :(");
+  }
+
     return (
       <>
         <div className="container">
@@ -15,6 +53,24 @@ export default function Map() {
                 <div className="title">
                     <h1>INVITATIONS</h1>
                 </div>
+                <table className="invitations-table">
+                  <tr>
+                    <th>From</th>
+                    <th>Group Name</th>
+                    <th></th>
+                    <th></th>
+                  </tr>
+                    {invitations.length === 0 && <tr>No Invitations.</tr>}
+                    {invitations && invitations.map(invitation =>{
+                      return(
+                      <tr>
+                        <td>{invitation.email}</td>
+                        <td>test</td>
+                        <td><button onClick={acceptInvitation}>Accept</button></td>
+                        <td><button onClick={denyInvitation}>Deny</button></td>
+                      </tr>)
+                    })} 
+                </table>
               </div>
           </div>
         </div>
