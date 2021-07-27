8234a5f908a2c85c3fa1a0beccb834e37d09b913
Vijay Stroup <vijaystroup@gmail.com>
2021-07-27 16:00:19 -0400

    update auth context for login and signup

--- mobile/context/AuthContext.js
+++ mobile/context/AuthContext.js
@@ -1,5 +1,4 @@
-import React from 'react'
-import { createContext, useContext, useEffect, useState } from 'react'
+import React, { createContext, useContext, useEffect, useState } from 'react'
 import { auth } from '../firebase'
 
 const AuthContext = createContext()
@@ -12,10 +11,61 @@ export function AuthProvider({ children }) {
   const [user, setUser] = useState()
   const [loading, setLoading] = useState(true)
 
-  function signup(email, password) {
+  function toTitleCase(str) {
+    return str.replace(
+      /\w\S*/g,
+      function(txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+      }
+    )
+  }
+
+  function updateLoc(lat, lon) {
+    const data = {}
+    const dbRequest = window.indexedDB.open('firebaseLocalStorageDb')
+    dbRequest.onsuccess = ev => {
+      const db = ev.target.result
+      const tx = db.transaction('firebaseLocalStorage', 'readonly')
+      const store = tx.objectStore('firebaseLocalStorage')
+      const getReq = store.getAll()
+
+      getReq.onsuccess = ev => {
+        const target = ev.target.result[0].value
+        data['email'] = target.email
+        data['userId'] = target.uid
+        data['auth'] = target.stsTokenManager.accessToken
+        data['expiration'] = target.stsTokenManager.expirationTime
+        data['lat'] = lat
+        data['lon'] = lon
+
+        if (data.userId && target.emailVerified) {
+          fetch('https://group20-midpoint.herokuapp.com/api/login', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+          })
+        }
+      }
+    }
+  }
+
+  function signup(email, firstname, lastname, password) {
     return auth.createUserWithEmailAndPassword(email, password)
-      .then(user => {
+      .then(async user => {
         user.user.sendEmailVerification()
+        await fetch('https://group20-midpoint.herokuapp.com/api/register', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email,
+            firstname: toTitleCase(firstname),
+            lastname: toTitleCase(lastname)
+          })
+        })
       })
   }
 
@@ -34,7 +84,30 @@ export function AuthProvider({ children }) {
   }
 
   useEffect(() => {
-    const unsub = auth.onAuthStateChanged(user => setUser(user))
+    const unsub = auth.onAuthStateChanged(user => {
+      setUser(user)
+      if (user) {
+        // https://stackoverflow.com/questions/11747440/wait-for-callback-in-javascript
+        let geoloc
+        const successful = function (position) {
+          geoloc = {
+            longitude: position.coords.longitude,
+            latitude: position.coords.latitude
+          }
+        }
+
+        const getLocation = function(callback) {
+          navigator.geolocation.getCurrentPosition(pos => {
+            successful(pos)
+            typeof callback === 'function' && callback(geoloc)
+          }, null, { enableHighAccuracy: true })
+        }
+
+        getLocation(function(pos){
+          updateLoc(pos.latitude, pos.longitude)
+        })
+      }
+    })
     setLoading(false)
 
     return unsub
