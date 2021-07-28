ccec8f07996ab10ecd656b3d3509663ab860e3ef
Devin Besaw <devinbesaw15@yahoo.com>
2021-07-27 23:24:13 -0400

    Invitations now work. Styled buttons.
    Co-authored-by: Vijay Stroup <VijayStroup@users.noreply.github.com>

--- web/src/components/invitations.jsx
+++ web/src/components/invitations.jsx
@@ -2,46 +2,77 @@ import React, { useEffect, useState } from "react";
 import SideBar from "./sidebar"
 import "../styles/invitations.scss";
 import { useAuth } from "../contexts/authContext";
-import { getDynamicStyles } from "jss";
-
-
+import CheckIcon from '@material-ui/icons/Check';
+import CloseIcon from '@material-ui/icons/Close';
 
 export default function Invitations() {
   const { user } = useAuth();
-  const [invitations, setInvitations] = useState([
-    {
-      email: "test@gmail.com"
-    },
-    {
-      email: "test@gmail.com"
-    }
-  ]);
+  const [invitations, setInvitations] = useState([]);
 
-  // useEffect(() => {
-  //   fetch('https://group20-midpoint.herokuapp.com/api/listinvites', {
-  //     method: "POST",
-  //     headers: {
-  //       'Accept': 'application/json',
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify({
-  //         userId : user.uid,
-  //         userToken : user.Aa,
-  //         email: user.email,
-  //       })
-  //   })
-  //   .then(response => response.json())
-  //   .then(data => {console.log(data);setInvitations(data.invitedata)})
-  // }, []);
+  useEffect(() => {
+    fetch('https://group20-midpoint.herokuapp.com/api/listinvites', {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+          userId : user.uid,
+          userToken : user.Aa,
+          email: user.email,
+        })
+    })
+    .then(response => response.json())
+    .then(data => {setInvitations(data.invitedata)})
+  }, []);
 
-  function acceptInvitation()
+  function acceptInvitation(index, id, gid)
   {
-    console.log("invitation accepted :)");
+    fetch('https://group20-midpoint.herokuapp.com/api/acceptinvitation', {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+          userId : user.uid,
+          userToken : user.Aa,
+          email: user.email,
+          inviteId: id,
+          groupId: gid,
+        })
+    })
+
+    .then(response => {
+      if (response.ok)
+      {
+        setInvitations(invitations.filter((invitation, i) => i !== index))
+      }
+    })
   }
 
-  function denyInvitation()
+  function declineInvitation(index, id)
   {
-    console.log("invitation denied :(");
+    fetch('https://group20-midpoint.herokuapp.com/api/declineinvitation', {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+          userId : user.uid,
+          userToken : user.Aa,
+          email: user.email,
+          inviteId: id
+        })
+    })
+
+    .then(response => {
+      if (response.ok)
+      {
+        setInvitations(invitations.filter((invitation, i) => i !== index))
+      }
+    })
   }
 
     return (
@@ -54,22 +85,28 @@ export default function Invitations() {
                     <h1>INVITATIONS</h1>
                 </div>
                 <table className="invitations-table">
-                  <tr>
-                    <th>From</th>
-                    <th>Group Name</th>
-                    <th></th>
-                    <th></th>
-                  </tr>
-                    {invitations.length === 0 && <tr>No Invitations.</tr>}
-                    {invitations && invitations.map(invitation =>{
-                      return(
+                  {invitations.length === 0 ? <thead><tr><th id="no-invitations" colSpan="2">No Invitations</th></tr></thead> : 
+                    <thead>
                       <tr>
-                        <td>{invitation.email}</td>
-                        <td>test</td>
-                        <td><button onClick={acceptInvitation}>Accept</button></td>
-                        <td><button onClick={denyInvitation}>Deny</button></td>
-                      </tr>)
-                    })} 
+                        <th>Group Name</th>
+                        <th>From</th>
+                        <th></th>
+                        <th></th>
+                      </tr>
+                    </thead>
+                  }
+                  <tbody>
+                      {invitations && invitations.map((invitation, i) =>{
+                        return(
+                          <tr>
+                            <td>{invitation.groupname}</td>
+                            <td>{invitation.from}</td>
+                            <td align="center"><button id="accept-button" onClick={() => acceptInvitation(i, invitation.inviteId, invitation.groupId)}><CheckIcon id="check-icon"/>Accept</button></td>
+                            <td align="center"><button id="decline-button" onClick={() => declineInvitation(i, invitation.inviteId)}><CloseIcon id="close-icon"/>Decline</button></td>
+                          </tr>
+                        )
+                      })}
+                  </tbody>
                 </table>
               </div>
           </div>
