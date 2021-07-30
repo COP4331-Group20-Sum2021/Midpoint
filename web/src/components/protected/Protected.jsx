c2d645f62647ff994a7d234c5a51a6702a6da7eb
Vijay Stroup <vijaystroup@gmail.com>
2021-07-30 17:25:01 -0400

    fix signup verify page

--- web/src/components/protected/Protected.jsx
+++ web/src/components/protected/Protected.jsx
@@ -1,5 +1,6 @@
 import { Redirect, Route } from 'react-router-dom'
 import { useAuth } from '../../contexts/authContext'
+import Verify from '../verify'
 
 export default function Protected({ component: Component, ...rest }) {
   const { user } = useAuth()
@@ -8,10 +9,14 @@ export default function Protected({ component: Component, ...rest }) {
     <Route
       {...rest}
       render={props => {
+        console.log(props.location.pathname)
         if (user && user.emailVerified) {
           return <Component {...props} />
         } else if (user && !user.emailVerified) {
-          return <Redirect to='/verify' />
+          if (props.location.pathname === '/verify')
+            return <Verify /> 
+          else
+            return <Redirect to='/verify' />
         } else {
           return <Redirect to='/login' />
         }
