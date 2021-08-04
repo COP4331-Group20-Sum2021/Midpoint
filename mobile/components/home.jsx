8ad50b94f3ac96769e094bcecd6438e28c9d0394
Vijay Stroup <vijaystroup@gmail.com>
2021-08-04 16:04:42 -0400

    home page content update

--- mobile/components/home.jsx
+++ mobile/components/home.jsx
@@ -15,31 +15,25 @@ export default function Home({ navigation }) {
                         <Image style={styles.picture} source={createGroupsPicture}/>
                         <Text style={styles.title}>Create Groups</Text>
                         <Text style={styles.description}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute ir
-                            ure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excep
-                            teur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            Planning a night out with friends but cant decide where to go? Want to get dinner with your significant other but neither of you 
+                            can decide which establishment is for you? Well Midpoint is the app for you! Midpoint is an application to make getting together 
+                            with friends easier. Sign up and create a group through our interface. From there, midpoint will get everyone's current locations, 
+                            and it will give you a list of places in the middle of all of you.
                         </Text>
                         <View style={styles.seperator}/>
                         <Image style={styles.picture} source={findMidpointsPicture}/>
                         <Text style={styles.title}>Find Midpoints</Text>
                         <Text style={styles.description}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute ir
-                            ure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excep
-                            teur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            Midpoint will automatically create a circle in the center of a group to find establishments of equal distance from all members. 
+                            Then, midpoint will grab all of those establishments in the circle and return a list of them to you with their name, address, and rating.
                         </Text>
                         <View style={styles.seperator}/>
                         <Image style={styles.picture} source={meetUpPicture}/>
                         <Text style={styles.title}>Meet Up</Text>
                         <Text style={styles.description}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute ir
-                            ure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excep
-                            teur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            Midpoint will give you a list of various establishments to meet up at. Those establishments can be sorted by type to filter out places your
+                            group might not what to go to. Restaurants, entertainment, recreation, and shopping can all be filtered to find the perfect place for your 
+                            group to meet up. Additionally, ratings can be seen for each establishment to determine places not worth going to.
                         </Text>
                         <View style={{padding: 20}}/>
                     </View>
